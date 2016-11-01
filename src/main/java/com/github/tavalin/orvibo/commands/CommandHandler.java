8b404b68750a7eb56490deb624539f35f45f4386
Daniel Walters <danielwalters1@hotmail.com>
2016-11-01 00:04:00 +0000

    Initial network refactor
    Signed-off-by: Daniel Walters <danielwalters1@hotmail.com>

--- src/main/java/com/github/tavalin/orvibo/commands/CommandHandler.java
+++
@@ -1,9 +0,0 @@
-package com.github.tavalin.orvibo.commands;
-
-import com.github.tavalin.orvibo.protocol.Message;
-
-public interface CommandHandler {
-
-    public void handle(Message message);
-}
- 
